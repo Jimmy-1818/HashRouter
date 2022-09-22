18f428735967b40a66983c3ee2acb0a3373ac574
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-09-22 22:03:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ var link = window.location.href
 var sublink = link.slice(42)
 
 if (sublink.length > 10){
-	document.getElementById("link").innerHTML = sublink};
-	alert(sublink)
+	document.getElementById("link").innerHTML = sublink
+	alert(sublink)};
