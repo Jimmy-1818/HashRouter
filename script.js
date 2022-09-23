14809e50cc9f66c7323c0984430a6bdbb89e143b
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-09-23 13:54:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var link = window.location.href
 var sublink = link.slice(42)
 
 if (sublink.length > 10){
+	alert("changed")
 	document.getElementById("added-text").innerHTML = sublink
 	var current_link = document.getElementById('link')
 	current_link.setAttribute('href', 'sublink')};
