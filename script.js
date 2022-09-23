20ddd44c0f66794621dd16fe94f096a3af8704fe
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-09-23 16:54:24 +0200

    a

--- script.js
+++ script.js
@@ -5,8 +5,9 @@
 var link = window.location.href
 var sublink = link.slice(42)
 
-// change link in subscription when receiving a personalized url
+// change registration link when receiving a personalized url
 if (sublink.length > 10){
+	console.log("in > 10")
 	document.getElementById("added-text").innerHTML = sublink
 	var current_link = document.getElementById('link')
 	current_link.setAttribute('href', sublink)};
@@ -18,9 +19,12 @@ if (sublink.length > 10){
 
 document.getElementById("process-link").addEventListener("click", process_link);
 
+function process_link(){
+	//if(non vuoto){tutto}
+	var affiliation_link = document.getElementById("link-to-process").value
+	var variable_affiliation = String(affiliation_link).slice(29)
+	var affiliate_site = "https://jimmy-1818.github.io/HashRouter/" + "#/" + variable_affiliation
 
-function process_link{
-	//if(){}
-	var raw_link = document.getElementById("link-to-process")
-	document.getElementById("processed-link").innerHTML(raw_link.slice(29))
-};
+	document.getElementById("processed-link").value = affiliate_site};
+
+	// else vedi che messaggio rendere
