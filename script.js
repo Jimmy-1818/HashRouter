f22ce941037a51873d28c5b5f46ce83d8118c013
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-09-23 14:58:13 +0200

    g

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ if (sublink.length > 10){
 	document.getElementById("added-text").innerHTML = sublink
 	var current_link = document.getElementById('link')
 	current_link.setAttribute('href', sublink)};
+
+
+document.getElementById("process-link").addEventListener("click", process_link(document.getElementById("link-to-process")));
+
+
+function process_link(sub){
+
+
+document.getElementById("processed-link").innerHTML(MMMMMMMMMMMM)
+}
