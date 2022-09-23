c629317dc43add849352eb9c5c25949400bb69ff
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-09-23 15:15:11 +0200

    j

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 
 
+// SU CARICAMENTO
+// pick up the current url > variable part
 var link = window.location.href
 var sublink = link.slice(42)
 
+// change link in subscription when receiving a personalized url
 if (sublink.length > 10){
-	alert("changed")
 	document.getElementById("added-text").innerHTML = sublink
 	var current_link = document.getElementById('link')
 	current_link.setAttribute('href', sublink)};
 
 
-document.getElementById("process-link").addEventListener("click", process_link(document.getElementById("link-to-process")));
 
 
-function process_link(raw_link){
+
+
+document.getElementById("process-link").addEventListener("click", process_link);
+
+
+function process_link{
 	//if(){}
+	var raw_link = document.getElementById("link-to-process")
 	document.getElementById("processed-link").innerHTML(raw_link.slice(29))
 };
